ed88476bf8f4d4be647a2543470870baa7e63181
Letsifer <gavrichenkov_je@mail.ru>
2016-03-22 20:14:19 +0600

    add method which pushes all to right add test for this method

--- src/logic/Structure.java
+++ src/logic/Structure.java
@@ -5,31 +5,87 @@ package logic;
  * @author Евгений
  */
 public class Structure {
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("Structure with size = ");
+        builder.append(SIZE);
+        builder.append('\n');
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                builder.append(field[i][j]).append(' ');
+            }
+            builder.append('\n');
+        }
+        return builder.toString();
+    }
+
     private final int SIZE;
     private final int MAX_POINTS;
     private int[][] field;
+    private int points = 0;
 
     int getCell(int i, int j) {
         return field[i][j];
     }
     
-    public Structure(int size, int max_points) {
+    void setValue(int i, int j, int value) {
+        field[i][j] = value;
+    }
+    
+    /**
+     * Создает экземпляр структуры произольного размера и количество очков =
+     * 256.
+     *
+     * @param size размер поля
+     */
+    public Structure(int size) {
         this.SIZE = size;
-        this.MAX_POINTS = max_points;
+        this.MAX_POINTS = 512;
         field = new int[size][size];
     }
-    
+
+    /**
+     * Создает экземпляр структуры произольного размера и произвольным
+     * количеством очков
+     *
+     * @param size размер поля
+     * @param maxPoints максимальное количество очков до победы
+     */
+    public Structure(int size, int maxPoints) {
+        this.SIZE = size;
+        this.MAX_POINTS = maxPoints;
+        field = new int[size][size];
+    }
+
+    /**
+     * Сдвигает все цифры вправо на 1 клетку, склеивая их при равенстве.
+     */
     public void pushRight() {
-        
+        for (int i = 0; i < SIZE - 1; i++) {
+            for (int j = SIZE - 2; j >= 0; j--) {
+                if (field[i][j + 1] == 0) {
+                    field[i][j + 1] = field[i][j];
+                    field[i][j] = 0;
+                } else if (field[i][j] == field[i][j + 1]) {
+                    field[i][j + 1] *= 2;
+                    field[i][j] = 0;
+                    points += field[i][j + 1];
+                }
+            }
+        }
     }
+
     public void pushLeft() {
-        
+
     }
+
     public void pushDown() {
-        
+
     }
+
     public void pushUp() {
-        
+
     }
-    
+
 }
--- test/logic/StructureTest.java
+++ test/logic/StructureTest.java
@@ -18,10 +18,26 @@ public class StructureTest {
     @Test
     public void testPushRight() {
         System.out.println("pushRight");
-        Structure instance = null;
+        final int size = 4;
+        Structure instance = new Structure(size);
+        instance.setValue(0, 0, 4);
+        instance.setValue(0, 1, 4);
+        instance.setValue(0, 2, 4);
+        instance.setValue(1, 2, 2);
+        instance.setValue(1, 3, 2);
         instance.pushRight();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Structure sample = new Structure(size);
+        sample.setValue(0, 1, 4);
+        sample.setValue(0, 2, 4);
+        sample.setValue(0, 3, 4);
+        sample.setValue(1, 3, 4);
+        System.err.println(instance);
+        System.err.println(sample);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                assertTrue(instance.getCell(i, j) == sample.getCell(i, j));
+            }
+        }
     }
 
     /**
